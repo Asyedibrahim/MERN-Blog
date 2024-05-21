64ead117bc011bc55bb0b0756f4702768d93cf3b
Asyedibrahim <syedibrahim7252@gmail.com>
2024-05-21 16:44:26 +0530

    create api route for signup and handling middleware

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js'
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -16,5 +17,19 @@ app.listen(3000, () => {
     console.log('Server is running in port 3000')
 });
 
+app.use(express.json());
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+// Middleware function
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Invalid server error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
