78bce1568c6d0c99f304959aedffa923e0fd42e7
Asyedibrahim <syedibrahim7252@gmail.com>
2024-05-21 12:50:59 +0530

    complete connect and run server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
   return (
     <Navbar className='border-b-2'>
         <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>JoyBoy's</span>Blog
+            <span className='px-2 py-1 bg-gradient-to-br from-green-400 to-cyan-600 rounded-lg text-white'>JoyBoy's</span>Blog
         </Link>
         <form>
             <TextInput 
@@ -25,7 +25,7 @@ export default function Header() {
 
         <div className="flex gap-2 md:order-2">
             <Link to='/sign-in' className='w-13'>
-                <Button gradientDuoTone='purpleToPink'>Sign in</Button>
+                <Button outline gradientDuoTone='greenToBlue'>Sign in</Button>
             </Link>
             <Navbar.Toggle/>
         </div>
