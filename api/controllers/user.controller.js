2f8916ef853e5f99dca714fcbb959eb1fc9f17c7
Asyedibrahim <syedibrahim7252@gmail.com>
2024-05-23 12:30:07 +0530

    create user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,56 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from 'bcryptjs';
+
+
 export const test = (req, res) => {
     res.json({message: 'Api is working'})
-}
+};
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.userId) {
+        return next(errorHandler(401, 'You can only update your own account!'));
+    }
+
+    if (req.body.password) {
+        if(req.body.password.length < 6) {
+            return next(errorHandler(400, 'Password must be atleast 6 characters'))
+        }
+        req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    if (req.body.username) {
+        if (req.body.username.length < 7 || req.body.username.length > 20) {
+            return next(errorHandler(400, 'Username must be between 7 and 20 characters'))
+        }
+        if (req.body.username.includes(' ')) {
+            return next(errorHandler(400, 'Username cannot contains spaces'))
+        }
+        if (req.body.username !== req.body.username.toLowerCase()) {
+            return next(errorHandler(400, 'Username must be lowercase'))
+        }
+        if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+            return next(errorHandler(400, 'Username can only contains letter and numbers'))
+        }
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.userId ,{
+            $set: {
+                username: req.body.username,
+                email: req.body.email, 
+                password: req.body.password,
+                avatar: req.body.avatar
+            },
+        }, { new: true });
+
+        const { password: pass, ...rest } = updatedUser._doc;
+        res.status(200).json(rest)
+    } catch (error) {
+        if (error.code === 11000) {
+            next(errorHandler(403, 'Username or Email is already exists'))
+        } else {
+            next(error);
+        }
+    }
+};
