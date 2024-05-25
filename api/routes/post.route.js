017b557af764876dd21e7bdc81a181ed4db412be
Asyedibrahim <syedibrahim7252@gmail.com>
2024-05-25 12:51:00 +0530

    add delete post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js'
-import { createPost, getPosts } from '../controllers/post.contoller.js';
+import { createPost, getPosts, deletePost } from '../controllers/post.contoller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createPost);
 router.get('/getposts', getPosts );
+router.delete('/deletepost/:postId/:userId',verifyToken, deletePost);
 
 export default router;
--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -2,22 +2,25 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Table } from 'flowbite-react';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 
 export default function DashPosts() {
 
   const { currentUser } = useSelector((state) => state.user);
-  const [userPost, setUserPost] = useState([]);
+  const [userPosts, setUserPosts] = useState([]);
   const [showMore, setShowMore] = useState(true);
+  const [postIdToDelete, setPostIdToDelete] = useState('');
 
   
   useEffect(()=> {
+    window.scrollTo(0, 0);
     const fetchPost = async () => {
       try {
         const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
         const data = await res.json();
         if (res.ok) {
-          setUserPost(data.posts);
+          setUserPosts(data.posts);
           if (data.posts.length < 9){
             setShowMore(false)
           }
@@ -34,12 +37,12 @@ export default function DashPosts() {
   }, [currentUser._id]);
 
   const handleShowMore = async () => {
-    const startIndex = userPost.length;
+    const startIndex = userPosts.length;
     try {
       const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
       const data = await res.json();
       if (res.ok) {
-        setUserPost((prev) => [...prev, ...data.posts]);
+        setUserPosts((prev) => [...prev, ...data.posts]);
         if (data.posts.length < 9) {
           setShowMore(false)
         }
@@ -50,9 +53,40 @@ export default function DashPosts() {
   };
 
 
+  const handleDeletePost = async (postId) => {
+    try {
+      const result = await Swal.fire({
+        title: 'Are you sure?',
+        text: 'Your account and all the listing will be delete!',
+        icon: 'error',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'Cancel !'
+      });
+  
+      if (result.isConfirmed) {
+        const res = await fetch(`/api/post/deletepost/${postId}/${currentUser._id}`, {
+            method: 'DELETE',
+          });
+        const data = await res.json();
+        if (!res.ok) {
+          console.log(data.message);
+        } else {
+          setUserPosts((prev) => prev.filter((post) => post._id !== postId)
+          );
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500 '>
-      {currentUser.isAdmin && userPost.length > 0 ? (
+      {currentUser.isAdmin && userPosts.length > 0 ? (
         <>
           <Table hoverable className='shadow-md'>
             <Table.Head>
@@ -65,7 +99,7 @@ export default function DashPosts() {
                 <span>Edit</span>
               </Table.HeadCell>
             </Table.Head>
-            {userPost.map((post, index) => (
+            {userPosts.map((post, index) => (
               <Table.Body className='divide-y' key={index}>
                 <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800' >
                   <Table.Cell>{new Date(post.updatedAt).toLocaleDateString()}</Table.Cell>
@@ -79,7 +113,8 @@ export default function DashPosts() {
                   </Table.Cell>
                   <Table.Cell>{post.category}</Table.Cell>
                   <Table.Cell>
-                    <span className='text-red-500 font-medium hover:underline cursor-pointer'>Delete</span>
+                    <span className='text-red-500 font-medium hover:underline cursor-pointer' 
+                    onClick={() => handleDeletePost(post._id)}>Delete</span>
                   </Table.Cell>
                   <Table.Cell className='text-teal-500 font-medium hover:underline'>
                     <Link to={`update-post/${post.slug}`}>Edit</Link>
